53f42929daff1c9fc65533d9e0d5cb2ab9857631
newmohib <@@mohib100@@>
2025-02-08 13:15:27 +0600

    added fron end server as build

--- src/index.js
+++ src/index.js
@@ -1,23 +1,48 @@
-const express = require('express');
-const { PORT } = require('./config');
-const { databaseConnection } = require('./database');
-const expressApp = require('./express-app');
+const express = require("express");
+const path = require("path");
+const { PORT } = require("./config");
+const { databaseConnection } = require("./database");
+const expressApp = require("./express-app");
 
-const StartServer = async() => {
+const StartServer = async () => {
+  const app = express();
 
-    const app = express();
-    
-    await databaseConnection.init();
-    
-    await expressApp(app);
+  await databaseConnection.init();
 
-    app.listen(PORT, () => {
-        console.log(`listening to port ${PORT}`);
+  await expressApp(app);
+
+  app
+    .listen(PORT, () => {
+      console.log(`listening to port ${PORT}`);
     })
-    .on('error', (err) => {
-        console.log(err);
-        process.exit();
-    })
-}
+    .on("error", (err) => {
+      console.log(err);
+      process.exit();
+    });
+};
 
 StartServer();
+
+const StartFrontendServer = async () => {
+  const app = express();
+  const PORT = process.env.ADMIN_END_PORT || 8001;
+
+  // Serve the static files from the React app
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  // Catch-all handler to serve React's index.html for unknown routes
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  });
+
+  app
+    .listen(PORT, () => {
+      console.log(`listening to port ${PORT}`);
+    })
+    .on("error", (err) => {
+      console.log(err);
+      process.exit();
+    });
+};
+
+StartFrontendServer();
